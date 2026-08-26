8253f5ec40120b59dac4f5d1a7fa3c36b4b5c9ea
Eva Raimundez <nepeta@gmail.com>
2026-08-26 20:31:52 +0100

    implemented nonop infrastructure classes

---
+++ src/GtMotive.Estimate.Microservice.Infrastructure/Bus/NoOpBus.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using GtMotive.Estimate.Microservice.Domain.Interfaces;
+
+namespace GtMotive.Estimate.Microservice.Infrastructure.Bus
+{
+    /// <summary>
+    /// No-operation implementation of <see cref="IBus"/> for development or testing.
+    /// </summary>
+    public sealed class NoOpBus : IBus
+    {
+        /// <inheritdoc />
+        public Task Send(object message) => Task.CompletedTask;
+    }
+}
---
+++ src/GtMotive.Estimate.Microservice.Infrastructure/Persistence/NoOpUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using GtMotive.Estimate.Microservice.Domain.Interfaces;
+
+namespace GtMotive.Estimate.Microservice.Infrastructure.Persistence
+{
+    /// <summary>
+    /// No-operation implementation of <see cref="IUnitOfWork"/> for development or testing.
+    /// </summary>
+    public sealed class NoOpUnitOfWork : IUnitOfWork
+    {
+        /// <inheritdoc />
+        public Task<int> Save() => Task.FromResult(0);
+    }
+}
